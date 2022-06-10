9d950a3191623c1d32429e97427dfb3beafba003
Mário Kolenič <mkolenicml@gmail.com>
2022-06-10 11:57:38 +0200

    Solved every move in 1ms

--- src/QueenProblem.java
+++ src/QueenProblem.java
@@ -1,37 +1,37 @@
 import java.util.ArrayList;
 
 public class QueenProblem {
-    private static int insertCounter = 0;
-    private static ArrayList<char[][]> successfullBoards = new ArrayList<>();
+    private static ArrayList<int[][]> successfullBoards = new ArrayList<>();
 
     public static void main(String[] args) {
-        char[][] gameBoard = new char[8][8];
+        int[][] gameBoard = new int[8][8];
 
-        printGameBoard(gameBoard);
+        for(int riadok = 0; riadok < gameBoard.length; riadok++) {
+            for (int stlpec = 0; stlpec < gameBoard[riadok].length; stlpec++) {
+                gameBoard[riadok][stlpec] = 0;
+            }
+        }
 
-        int queensCount = 0;
-        boolean successfull = false;
         long start = System.currentTimeMillis();
-        insertQueen(gameBoard, queensCount, successfull);
+        insertQueen(gameBoard, 0);
         long elapsedTimeMillis = System.currentTimeMillis()-start;
 
         printGameBoard(gameBoard);
 
-        System.out.println("Insert counter: " + insertCounter);
         System.out.println("In-time: " + elapsedTimeMillis + " ms");
         System.out.println("Number of Boards from list: " + successfullBoards.size());
 
-        for(char[][] board : successfullBoards) {
+        for(int[][] board : successfullBoards) {
             printGameBoard(board);
             System.out.println("\n");
         }
     }
 
-    static void printGameBoard(char[][] gameBoard) {
+    static void printGameBoard(int[][] gameBoard) {
         for(int riadok = 0; riadok < gameBoard.length; riadok++) {
             System.out.print((8-riadok) + " ");
             for(int stlpec = 0; stlpec < gameBoard[riadok].length; stlpec++) {
-                if(gameBoard[riadok][stlpec] == '♛') {
+                if(gameBoard[riadok][stlpec] == 1) {
                     System.out.print(" ♛ ");
                 }
                 else
@@ -42,154 +42,47 @@ public class QueenProblem {
         System.out.println("   a  b  c  d  e  f  g  h ");
     }
 
-    static boolean insertQueen(char[][] gameBoard, int queensCount, boolean successfull) {
-        if(!successfull) {
-            for(int riadok = 0; riadok < gameBoard.length; riadok++) {
-                for(int stlpec = 0; stlpec < gameBoard[riadok].length; stlpec++) {
-                    if(gameBoard[riadok][stlpec] == 0) {
-                        if(hasCleanMoves(gameBoard, riadok, stlpec)) {
-                            gameBoard[riadok][stlpec] = '♛';
-                            queensCount++;
-                            if(queensCount < 8) {
-                                successfull = insertQueen(gameBoard, queensCount, false);
+    static void insertQueen(int[][] gameBoard, int stlpec) {
+        if(stlpec >= gameBoard.length) {
+            int[][] newBoard = new int[8][8];
+            for(int x = 0; x < newBoard.length; x++) {
+                System.arraycopy(gameBoard[x], 0, newBoard[x], 0, newBoard[x].length);
+            }
+            successfullBoards.add(newBoard);
+        }
+        else {
+            for (int riadok = 0; riadok < gameBoard.length; riadok++) {
 
-                                if(!successfull) {
-                                    gameBoard[riadok][stlpec] = 0;
-                                    queensCount--;
-                                }
-                                else
-                                    return true;
-                            }
-                            else {
-                                for(char[][] board : successfullBoards) {
-                                    boolean equals = false;
-                                    for(int x = 0; x < board.length; x++) {
-                                        for (int y = 0; y < board[x].length; y++) {
-                                            if((board[x][y] == 0 && gameBoard[x][y] == 0) ||
-                                                    (board[x][y] == '♛' && gameBoard[x][y] == '♛')) {
-                                                equals = true;
-                                            }
-                                            else {
-                                                equals = false;
-                                                break;
-                                            }
-                                        }
-                                        if(!equals)
-                                            break;
-                                    }
+                gameBoard[riadok][stlpec] = 1;
 
-                                    if(equals) {
-                                        gameBoard[riadok][stlpec] = 0;
-                                        return false;
-                                    }
-                                }
-                                char[][] newBoard = new char[8][8];
-                                for(int x = 0; x < newBoard.length; x++) {
-                                    System.arraycopy(gameBoard[x], 0, newBoard[x], 0, newBoard[x].length);
-                                }
-
-                                successfullBoards.add(newBoard);
-                                gameBoard[riadok][stlpec] = 0;
-                                return false;
-                            }
-                        }
-                    }
+                if (hasCleanMoves(gameBoard, riadok, stlpec)) {
+                    insertQueen(gameBoard, (stlpec+1));
                 }
+                gameBoard[riadok][stlpec] = 0;
             }
         }
-        return successfull;
     }
 
-    static boolean hasCleanMoves(char[][] gameBoard, int queenRiadok, int queenStlpec) {
-        for(int riadok = 0; riadok < gameBoard.length; riadok++) {
-            for(int stlpec = 0; stlpec < gameBoard[riadok].length; stlpec++) {
-                if(gameBoard[riadok][stlpec] == '♛') {
-                    if(queenRiadok == riadok || queenStlpec == stlpec) {
-                        return false;
-                    }
-                }
+    static boolean hasCleanMoves(int[][] gameBoard, int queenRiadok, int queenStlpec) {
+        for (int stlpec = 0; stlpec < queenStlpec; stlpec++) {
+            if (gameBoard[queenRiadok][stlpec] == 1) {
+                return false;
             }
         }
 
-        return hasCleanDiagonal(gameBoard, queenRiadok, queenStlpec);
-    }
-
-    static boolean hasCleanDiagonal(char[][] gameBoard, int queenRiadok, int queenStlpec) {
-        int tempRiadok = queenRiadok;
-        int tempStlpec = queenStlpec;
-
-        boolean stopPridavanie = false;
-
-        // pravo hore
-        while(!stopPridavanie) {
-            tempRiadok--;
-            tempStlpec++;
-            if(tempRiadok >= 0 && tempStlpec < 8) {
-                if(gameBoard[tempRiadok][tempStlpec] == '♛') {
-                    return false;
-                }
+        for (int riadok = queenRiadok-1, stlpec = queenStlpec-1;
+             riadok >= 0 && stlpec >= 0; riadok--, stlpec--) {
+            if (gameBoard[riadok][stlpec] == 1) {
+                return false;
             }
-            else
-                stopPridavanie = true;
         }
-        // System.out.println("[" + queenRiadok + "][" + queenStlpec + "]  Pravo hore -> OK");
 
-
-        tempRiadok = queenRiadok;
-        tempStlpec = queenStlpec;
-        stopPridavanie = false;
-
-        // pravo dolu
-        while(!stopPridavanie) {
-            tempRiadok++;
-            tempStlpec++;
-            if(tempRiadok < 8 && tempStlpec < 8) {
-                if(gameBoard[tempRiadok][tempStlpec] == '♛') {
-                    return false;
-                }
+        for (int riadok = queenRiadok+1, stlpec = queenStlpec-1;
+             stlpec >= 0 && riadok < gameBoard.length; riadok++, stlpec--) {
+            if (gameBoard[riadok][stlpec] == 1) {
+                return false;
             }
-            else
-                stopPridavanie = true;
         }
-        // System.out.println("[" + queenRiadok + "][" + queenStlpec + "]  Pravo dolu -> OK");
-
-
-        tempRiadok = queenRiadok;
-        tempStlpec = queenStlpec;
-        stopPridavanie = false;
-
-        // vlavo dolu
-        while(!stopPridavanie) {
-            tempRiadok++;
-            tempStlpec--;
-            if(tempRiadok < 8 && tempStlpec >= 0) {
-                if(gameBoard[tempRiadok][tempStlpec] == '♛') {
-                    return false;
-                }
-            }
-            else
-                stopPridavanie = true;
-        }
-        // System.out.println("[" + queenRiadok + "][" + queenStlpec + "]  Vlavo dolu -> OK");
-
-
-        tempRiadok = queenRiadok;
-        tempStlpec = queenStlpec;
-        stopPridavanie = false;
-
-        // vlavo hore
-        while(!stopPridavanie) {
-            tempRiadok--;
-            tempStlpec--;
-            if(tempRiadok >= 0 && tempStlpec >= 0) {
-                if(gameBoard[tempRiadok][tempStlpec] == '♛') {
-                    return false;
-                }
-            }
-            else
-                stopPridavanie = true;
-        }
-        // System.out.println("[" + queenRiadok + "][" + queenStlpec + "]  Vlavo hore -> OK");
 
         return true;
     }
